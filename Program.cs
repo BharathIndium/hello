184c7348f7e32fc8d890342fb55f4c9372c77f9f
BharathIndium <92966882+BharathIndium@users.noreply.github.com>
2021-11-07 13:05:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace face_book
+{
+    class Program
+    {
+        
+
+			string firstname;
+			string lastname;
+			string emailid;
+			long mobileno;
+
+        public string Firstname { get => firstname; set => firstname = value; }
+		public string Lastname { get => lastname; set => lastname = value; }
+		public string Emailid { get => emailid; set => emailid = value; }
+		public long Mobileno { get => mobileno; set => mobileno = value; }
+
+		public override bool Equals(object obj)
+		{
+			FACEBOOK F = (FACEBOOK)obj;
+			return this.Emailid.Equals(F.Emailid) && this.Mobileno.Equals(F.Mobileno);
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+		public override string ToString()
+		{
+			return string.Format("Firstname : {0}\nLastName : {1}\nEmail : {2}\nMobileno: {3}", Firstname, Lastname, Emailid, Mobileno);
+		}
+    }
+}
